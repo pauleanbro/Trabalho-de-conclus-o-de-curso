4910cf098950e3b903f7cea9a4a6ff03d082ba62
Gustavo Lima <gustavo.lima@onnitech.com.br>
2023-05-21 23:10:41 -0300

    Alterações no jogo 3

--- src/screens/Modules/Modules1/Exercises/Ex3Md1/index.js
+++ src/screens/Modules/Modules1/Exercises/Ex3Md1/index.js
@@ -1,82 +1,121 @@
 import React, { useState } from "react";
-import { View } from "react-native";
-import {
-  Container,
-  ContainerItens,
-  TextItens,
-  WordsItens,
-  ContainerButton,
-  TextButton,
-  Border,
-  ContainerNegado,
-  TextButtonNegado,
-} from "./styles";
-import Grid from "../../../../../components/Jogos/HuntingWords/Grid";
-import WordList from "../../../../../components/Jogos/HuntingWords/WordList";
+import { StyleSheet, View, PanResponder, Dimensions } from "react-native";
+import Svg, { Circle, Line } from "react-native-svg";
+import { Container, LetrasMaiusculas, ContainerButton } from "./styles";
 
-const data = [
-  ["TRA", "TRE", "TRI", "TRO", "TRU"],
-  ["BA", "BE", "BI", "BO", "BU"],
-  ["LHA", "LHE", "LHI", "LHO", "LHU"],
-];
+const { width, height } =
+  Dimensions.get("window");
+
+const CirclePoint = ({ cx, cy }) => (
+  <Circle cx={cx} cy={cy} r="10" fill="black" />
+);
+
+const CirclePoint2 = ({ cx, cy }) => (
+  <Circle cx={cx} cy={cy} r="10" fill="yellow" />
+);
+
+const CirclePoint3 = ({ cx, cy }) => (
+  <Circle cx={cx} cy={cy} r="10" fill="blue" />
+);
+
+const CirclePoint4 = ({ cx, cy }) => (
+  <Circle cx={cx} cy={cy} r="10" fill="red" />
+);
 
 const Ex3Md1 = () => {
-  const [selectedLetters, setSelectedLetters] = useState([]);
-  const [undo, setUndo] = useState(false);
-  const [words, setWords] = useState([]);
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [lineDrawn, setLineDrawn] = useState(false);
 
-  const handleLetterPress = (row, col) => {
-    setSelectedLetters([...selectedLetters, { row, col }]);
-    setUndo(false);
-  };
+  const verticalCenter = height / 9;
+  const startPoint = { x: 50, y: verticalCenter };
+  const endPoint = { x: width - 50, y: verticalCenter };
+  
+  const verticalCenter2 = height / 9 + 120;
+  const startPoint2 = { x: 50, y: verticalCenter2 };
+  const endPoint2 = { x: width - 50, y: verticalCenter2 };
 
-  const handleUndo = () => {
-    setSelectedLetters(selectedLetters.slice(0, -1));
-    setUndo(true);
-  };
+  const verticalCenter3 = height / 2;
+  const startPoint3 = { x: 50, y: verticalCenter3 };
+  const endPoint3 = { x: width - 50, y: verticalCenter3 };
 
-  const handleSave = () => {
-    const selectedWord = selectedLetters
-      .map((letter) => data[letter.row][letter.col])
-      .join("");
-    if (selectedWord !== "") {
-      setWords([...words, selectedWord]);
-      setSelectedLetters([]);
-    }
-  };
+  const verticalCenter4 = height / 2 + 120;
+  const startPoint4 = { x: 50, y: verticalCenter4 };
+  const endPoint4 = { x: width - 50, y: verticalCenter4 };
+  
+  const touchRadius = 30;
 
-  const selectedWord = selectedLetters
-    .map((letter) => data[letter.row][letter.col])
-    .join("");
+  const panResponder = PanResponder.create({
+    //retorna verdadeiro para indicar que este componente deve se tornar o respondente para este evento de toque.
+    onStartShouldSetPanResponder: () => true,
 
+    //é chamado quando o toque começa. Se o toque começa perto do ponto inicial, currentPosition é definido como o ponto inicial.
+    onPanResponderGrant: (event, gestureState) => {
+      if (
+        Math.abs(gestureState.x0 - startPoint.x) < 20 &&
+        Math.abs(gestureState.y0 - startPoint.y) < 20 
+      ) {
+        setCurrentPosition({ x: gestureState.x0, y: gestureState.y0 });
+      }
+    },
+
+    //é chamado quando o usuário move o dedo pela tela. Atualiza currentPosition para a localização atual do toque.
+    onPanResponderMove: (event, gestureState) => {
+      setCurrentPosition({ x: gestureState.moveX, y: gestureState.moveY });
+    },
+
+    //é chamado quando o usuário levanta o dedo da tela. Se o toque terminar perto do ponto final,
+    //lineDrawn é definido como true. Caso contrário, currentPosition é resetado para null, apagando a linha temporária.
+    onPanResponderRelease: (event, gestureState) => {
+      if (Math.abs(gestureState.moveX - endPoint.x) < 20 && Math.abs(gestureState.moveY - endPoint.y) < 20) {
+        setLineDrawn(true);
+      } else {
+        setCurrentPosition(null);
+      }
+    },
+  });
+
+  console.log(startPoint.x);
+  console.log(startPoint.y);
+  console.log(currentPosition);
+  console.log(
+    "######################################################################################"
+  );
+  console.log(endPoint.x);
+  console.log(endPoint.y);
+  
   return (
-    <Container>
-      <ContainerItens>
-        <Grid data={data} onLetterPress={handleLetterPress} />
-      </ContainerItens>
-      <WordList word={selectedWord} onUndo={handleUndo} onSave={handleSave} />
-      <WordsItens>
-        {words.map((words, index) => (
-          <>
-            <TextItens key={index}>{words}</TextItens>
-            <Border />
-          </>
-        ))}
-      </WordsItens>
-      {words.length >= 3 ? (
-        <View style={{ alignItems: "center" }}>
-          <ContainerButton>
-            <TextButton>Enviar</TextButton>
-          </ContainerButton>
-        </View>
-      ) : (
-        <View style={{ alignItems: "center" }}>
-          <ContainerNegado>
-            <TextButtonNegado>Enviar</TextButtonNegado>
-          </ContainerNegado>
-        </View>
-      )}
-    </Container>
+      <Container {...panResponder.panHandlers}>
+        <Svg height="100%" width="100%" viewBox={`0 0 ${width} ${height}`}>
+          <CirclePoint cx={startPoint.x} cy={startPoint.y} />
+          <CirclePoint cx={endPoint.x} cy={endPoint.y} />
+          <CirclePoint2 cx={startPoint2.x} cy={startPoint2.y} />
+          <CirclePoint2 cx={endPoint2.x} cy={endPoint2.y} />
+          <CirclePoint3 cx={startPoint3.x} cy={startPoint3.y} />
+          <CirclePoint3 cx={endPoint3.x} cy={endPoint3.y} />
+          <CirclePoint4 cx={startPoint4.x} cy={startPoint4.y} />
+          <CirclePoint4 cx={endPoint4.x} cy={endPoint4.y} />
+
+          {currentPosition && !lineDrawn && (
+            <Line
+              x1={startPoint.x}
+              y1={startPoint.y}
+              x2={currentPosition.x}
+              y2={currentPosition.y}
+              stroke="black"
+            />
+          )}
+
+          {lineDrawn && (
+            <Line
+              x1={startPoint.x}
+              y1={startPoint.y}
+              x2={endPoint.x}
+              y2={endPoint.y}
+              stroke="black"
+            />
+          )}
+        </Svg>
+      </Container>
   );
 };
 
--- src/screens/Modules/Modules1/Exercises/Ex3Md1/styles.js
+++ src/screens/Modules/Modules1/Exercises/Ex3Md1/styles.js
@@ -3,59 +3,22 @@ import styled from "styled-components/native";
 export const Container = styled.View`
   flex: 1;
   background-color: #ffffff;
-  justify-content: space-around;
 `;
 
-export const ContainerItens = styled.View`
-  padding: 40px 0px 30px 0px;
-  align-items: center;
-`;
-
-export const WordsItens = styled.View`
-  align-items: flex-start;
-`;
-
-export const ContainerNegado = styled.TouchableOpacity`
-  background-color: #CCCED3;
-  width: 230px;
-  height: 55px;
-  border-radius: 30px;
-  align-items: center;
-  justify-content: center;
+export const LetrasMaiusculas = styled.Text`
+   font-size: 26px;
+  color: #282828;
+  text-align: center;
   margin-bottom: 30px;
-`;
-
-export const TextButtonNegado = styled.Text`
-  font-size: 25px;
-  color: #000000;
+  font-family: 'Roboto';
 `;
 
 export const ContainerButton = styled.TouchableOpacity`
-  background-color: #5DB075;
+  background-color: #5db075;
   width: 230px;
   height: 55px;
   border-radius: 30px;
   align-items: center;
   justify-content: center;
   margin-bottom: 30px;
-`;
-
-export const TextButton = styled.Text`
-  font-size: 25px;
-  color: #ffffff;
-`;
-
-export const TextItens = styled.Text`
-  font-size: 20px;
-  color: #282828;
-  font-family: 'Roboto-Regular';
-  flex-wrap: wrap;
-  margin: 10px 0 0 10px;
-`;
-
-export const Border = styled.View`
-  width: 94%;
-  height: 1.5px;
-  background-color: #000000;
-  margin: 0 10px 0 10px;
 `;
